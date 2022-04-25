6841c95836e7523ed8126d0d17c111d4841ac710
Dead-Locke <impulse25@ymail.com>
2022-04-24 22:29:42 -0400

    Finalized all calculator functionality. Minor style changes and some buttons were moved around.

--- script.js
+++ script.js
@@ -7,59 +7,46 @@ const equals = document.getElementById('equals')
 const clearBtn = document.getElementById('clear')
 const backspace = document.getElementById('backspace')
 const decimal = document.getElementById('decimal')
+const negateBtn = document.getElementById('negate')
 
 const currDisplay = document.querySelector('.current')
 const prevDisplay = document.querySelector('.previous')
 
-
-var decimalPlaced = false;
 var zeroDivision = false;
 var currOperand;
 var prevOperand;
 var operator;
 
-
 numBtns.forEach(btn => btn.onclick =()=> append(btn.innerText)) //append every num to curentDisplay
-decimal.onclick =()=> {if(!decimalPlaced) {decimalPlaced = true; append(decimal.innerText)}}//only one decimal allowed
+decimal.onclick =()=> {if(!decimalPlaced()) { append(decimal.innerText)}}//only one decimal allowed
 oprBtns.forEach(btn => btn.onclick =()=> setOperator(btn))
+negateBtn.onclick = () => negate()
 equals.onclick = () => equal()
-
-
 backspace.onclick= () => currDisplay.innerText = currDisplay.innerText.slice(0,-1);
 clearBtn.onclick =() => clear();
 
 function setOperator(button) {
-    
-    if(operator){
-        if(zeroDivision){ 
-            dividedByZero();
-            return
-        }
+    if(operator != undefined && currDisplay.innerText != ''){
+        if(zeroDivision){ dividedByZero(); return}
         currOperand = +currDisplay.innerText
-        prevOperand = calculate(prevOperand, currOperand, operator); 
+        let answer = calculate(prevOperand, currOperand, operator).toFixed(4)
+        prevOperand = +answer 
     }
-    else if(currDisplay.innerText != ''){
-        prevOperand =  +currDisplay.innerText;
-    }
-    
-     operator = (button.id == 'exponent') ? '^' : button.innerText;
-     prevDisplay.innerText = prevOperand + operator;
-     currDisplay.innerText = '';
-     decimalPlaced = false;
+    else if(currDisplay.innerText != ''){ prevOperand =  +currDisplay.innerText;} 
 
+    operator = (button.id == 'exponent') ? '^' : button.innerText;
+    prevDisplay.innerText = prevOperand + operator;
+    currDisplay.innerText = '';
 }
 
-
 function equal() {
+    if(currDisplay.innerText == '' || operator == undefined ) return;
     currOperand = +currDisplay.innerText
-    let answer = calculate(prevOperand, currOperand, operator)
-    if(zeroDivision){ 
-        dividedByZero();
-        return
-    }
-    prevDisplay.innerText = `${prevOperand} ${operator} ${currOperand} = ${answer}`;
-    prevOperand = answer;
-    currDisplay.innerText = '';
+    let answer = calculate(prevOperand, currOperand, operator).toFixed(4)
+    if(zeroDivision){ dividedByZero(); return}
+    prevDisplay.innerText = `${prevOperand} ${operator} ${currOperand} =`;
+    prevOperand = +answer;
+    currDisplay.innerText = +answer;
     operator = undefined;      
 }
 
@@ -70,7 +57,6 @@ function append(value){
 function clear(){
     allButtons.forEach(btn => btn.disabled = false);
     zeroDivision = false;
-    decimalPlaced = false;
     prevDisplay.innerText =''
     currDisplay.innerText ='';
     operator = undefined; 
@@ -83,23 +69,28 @@ function calculate(a, b, opr){
     if(opr == '×') return a*b;
     if(opr == '^') return a**b;
     if(opr == '÷'){
-         if (b === 0){
-             zeroDivision = true;
-            return 
-            }
+        if (b === 0){zeroDivision = true; return }
         return a/b;
         }
 }
 
-function unary(a, opr){
-    if(opr = 'root') return Math.sqrt(a)
+function negate(){
+    if(currDisplay.textContent == '') return
+    if(currDisplay.textContent.includes('-')) currDisplay.textContent = currDisplay.textContent.slice(1);
+    else {currDisplay.textContent = '-' + currDisplay.textContent;}
 }
 
 function dividedByZero() {
     allButtons.forEach(btn => btn.disabled = true);
     clearBtn.disabled = false;
-    prevDisplay.innerText = `ERROR: Dividing by zero (${prevOperand} ${operator} ${currOperand})`;
-    currDisplay.innerText ='';
+    prevDisplay.innerText = `Divided by zero? Really?`;
+    currDisplay.innerText =`${prevOperand} ${operator} ${currOperand}`;
     return
+}
+
+function decimalPlaced(){
+   let num = currDisplay.innerText
+   if(num.includes('.')) return true; 
+   return false;
 }
 
